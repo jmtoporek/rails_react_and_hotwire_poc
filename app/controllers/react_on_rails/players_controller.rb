bde368969aec83e72454ec2dbd444e16ae1a66b2
Jared Toporek <jmtoporek@gmail.com>
2021-12-29 22:06:01 -0600

    Fix paths for react on rails

--- app/controllers/react_on_rails/players_controller.rb
+++ app/controllers/react_on_rails/players_controller.rb
@@ -1,5 +1,42 @@
 class ReactOnRails::PlayersController < PlayersController
 
+    def create
+      @player = Player.new(player_params)
+  
+      respond_to do |format|
+        if @player.save
+          format.html { redirect_to react_on_rails_player_url(@player), notice: "Player was successfully created." }
+          format.json { render :show, status: :created, location: @player }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @player.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # PATCH/PUT /players/1 or /players/1.json
+    def update
+      respond_to do |format|
+        if @player.update(player_params)
+          format.html { redirect_to react_on_rails_player_url(@player), notice: "Player was successfully updated." }
+          format.json { render :show, status: :ok, location: @player }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @player.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # DELETE /players/1 or /players/1.json
+    def destroy
+      @player.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to react_on_rails_players_url, notice: "Player was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    end
+
   def search
     if params[:query].present?
       @players = Player.where("name LIKE ?", "%#{params[:query]}%")
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
       end
     end
   end
+  namespace :react_on_rails do
+    resources :players do
+      collection do
+        get 'search'
+        post 'search'
+      end
+    end
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
