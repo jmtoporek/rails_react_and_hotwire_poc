13bf0166e76a88aff714929e679ae7b57a4fa1eb
Jared Toporek <jmtoporek@gmail.com>
2021-12-29 22:28:44 -0600

    Fix issue on index static page for turbo reload add hello controller just because

---
+++ client/controllers/hello_controller.js
@@ -0,0 +1,24 @@
+// Visit The Stimulus Handbook for more details 
+// https://stimulusjs.org/handbook/introduction
+// 
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "name", "output" ]
+
+  connect() {
+    console.log('in hello controller');
+    this.outputTarget.textContent = 'Hello, Stimulus!'
+  }
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
+  }
+}
