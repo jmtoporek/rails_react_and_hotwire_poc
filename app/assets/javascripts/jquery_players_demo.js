f4414e64ec9d81eeeed4cc43eb8b17af001fbbaa
Jared Toporek <jmtoporek@gmail.com>
2022-01-01 16:51:55 -0600

    Resuing views on scaffold for jquery add functions to players_helper replace all links to use helper functions page title is dynamic as well

--- app/assets/javascripts/jquery_players_demo.js
+++ app/assets/javascripts/jquery_players_demo.js
@@ -1,21 +1,17 @@
 // TODO: add debounce to input
 // TODO: move to webpack perhaps?
-// TODO: import jquery from here
+// TODO: import jquery from here?
 console.log('load the jquery demo');
-const playersFrameUrl = '/players/get_players_html?';
+const playersFrameUrl = '/players/get_players_html';
 $(function() {
-  if (document.location.href.includes('jquery') ) {
-    $("#page-title").text('jQuery as Hotwire Demo');
-
-    const $queryInput = $("#query");
-    $queryInput.keyup(function(event) {
-      let ajaxUrl = playersFrameUrl;
-      if (event.target.value.length > 0) {
-        ajaxUrl = ajaxUrl + `query=${event.target.value}`;
-      }
-      $.get( ajaxUrl, function( players_frame_html ) {
-        $("#players").replaceWith(players_frame_html );
-      });
+  const $queryInput = $("#query");
+  $queryInput.keyup(function(event) {
+    let ajaxUrl = playersFrameUrl;
+    if (event.target.value.length > 0) {
+      ajaxUrl = `${ajaxUrl}?query=${event.target.value}`;
+    }
+    $.get( ajaxUrl, function( players_frame_html ) {
+      $("#players").replaceWith(players_frame_html );
     });
-  }
+  });
 });
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,74 @@
 module PlayersHelper
+  def get_players_path
+    if params[:controller].starts_with?('jquery')
+      jquery_players_path
+    elsif params[:controller].starts_with?('react_on_rails')
+      react_on_rails_players_path
+    elsif params[:controller].starts_with?('hotwire')
+      hotwire_players_path
+    else
+      players_path
+    end
+  end
+
+  def get_player_path(player)
+    if params[:controller].starts_with?('jquery')
+      jquery_player_path(player.id)
+    elsif params[:controller].starts_with?('react_on_rails')
+      react_on_rails_player_path(player.id)
+    elsif params[:controller].starts_with?('hotwire')
+      hotwire_player_path(player.id)
+    else
+      player_path(player.id)
+    end
+  end
+
+  def get_edit_player_path(player)
+    if params[:controller].starts_with?('jquery')
+      edit_jquery_player_path(player.id)
+    elsif params[:controller].starts_with?('react_on_rails')
+      edit_react_on_rails_player_path(player.id)
+    elsif params[:controller].starts_with?('hotwire')
+      edit_hotwire_player_path(player.id)
+    else
+      edit_player_path(player.id)
+    end
+  end
+
+  def get_new_player_path
+    if params[:controller].starts_with?('jquery')
+      new_jquery_player_path
+    elsif params[:controller].starts_with?('react_on_rails')
+      new_react_on_rails_player_path
+    elsif params[:controller].starts_with?('hotwire')
+      new_hotwire_player_path
+    else
+      new_player_path
+    end
+  end
+
+  def get_search_players_path
+    if params[:controller].starts_with?('jquery')
+      search_jquery_players_path
+    # elsif params[:controller].starts_with?('react_on_rails')
+    #   search_react_on_rails_players_path
+    elsif params[:controller].starts_with?('hotwire')
+      new_hotwire_player_path
+    else
+      search_players_path
+    end
+  end
+
+  def get_page_title
+    if params[:controller].starts_with?('jquery')
+      "jQuery as Hotwire"
+    elsif params[:controller].starts_with?('react_on_rails')
+      "React on Rails"
+    elsif params[:controller].starts_with?('hotwire')
+      "Stimulus/Turbo Hotwire"
+    else
+      "Rails Scaffold"
+    end
+  end
+
 end
