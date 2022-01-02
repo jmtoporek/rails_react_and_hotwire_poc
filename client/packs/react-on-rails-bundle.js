864be49bcaaa4045b9711fc54a15ea4d9f6c6f24
Jared Toporek <jmtoporek@gmail.com>
2022-01-02 09:07:54 -0600

    Fix folder name

--- client/packs/react-on-rails-bundle.js
+++ client/packs/react-on-rails-bundle.js
@@ -1,5 +1,5 @@
 import ReactOnRails from 'react-on-rails';
-import PlayersSearchComponent from '../bundles/ReactOnRailsDemo/PlayersSearchComponent';
+import PlayersSearchComponent from '../bundles/react_on_rails_demo/PlayersSearchComponent';
 
 ReactOnRails.register({
   PlayersSearchComponent
